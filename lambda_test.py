743a4c77903c6db5ff87064d8fe80beb247ee540
agarcia <nsf-agarcia@Artemiss-MacBook-Pro.local>
2019-10-08 18:14:20 -0700

    Added file used to test lambda

---
+++ lambda_test.py
@@ -0,0 +1,31 @@
+import json
+import boto3
+import base64
+
+
+def clean_dict(data):
+    for key, value in list(data.items()):
+        if not value:
+            del data[key]
+        elif isinstance(value, dict):
+            clean_dict(value)
+    return data
+
+def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('tweets')
+
+    for record in event['Records']:
+        try:
+            payload = base64.b64decode(record["kinesis"]["data"])
+            data = json.loads(str(payload, 'UTF-8'))
+            clean_data = clean_dict(data)
+            table.put_item(
+                Item=clean_data
+            )
+            return {
+                'id_str': item.get('id_str'),
+                'text': item.get('text'),
+            }
+        except:
+            return 'Lambda failed'
